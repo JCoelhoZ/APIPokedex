58dd06be700d1ccc6f765727ed2238a87aebecda
Jaqueline Zacher <jaquezacher@gmail.com>
2018-12-01 00:02:50 -0200

    Update

--- src/main/java/model/Pokemon.java
+++ src/main/java/model/Pokemon.java
@@ -8,20 +8,25 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 import enums.Type;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name="pokemon")
 public class Pokemon {
+	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	@Column(name="id")
+	@Getter @Setter
 	private long id;
 	
 	@Column(name="type")
+	@Getter @Setter
 	private Type type;
 	
 	@Column(name="name")
+	@Getter @Setter
 	private String name;
-	
 	
 }
---
+++ src/main/java/service/PokemonService.java
@@ -0,0 +1,14 @@
+package service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import repository.PokemonRepository;
+
+public class PokemonService {
+
+	@Autowired
+	private PokemonRepository repository;
+	
+	
+	
+}
